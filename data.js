c069c8640ac7a707cbd5c10c712bac7b4274c29d
sebasmg20 <sebas_montes10@hotmail.com>
2024-05-29 23:09:43 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,528 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada-",
+      "name": "FACHADA-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.0206223595689021
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04717850705416993,
+          "pitch": 0.10340441975088055,
+          "rotation": 0,
+          "target": "3-sala-comedor-"
+        },
+        {
+          "yaw": -0.4387292089594119,
+          "pitch": 0.093804579654023,
+          "rotation": 0,
+          "target": "2-parqueadero-bbq-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parqueadero-zen-",
+      "name": "PARQUEADERO ZEN-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1507968980190295,
+          "pitch": 0.13683284587049016,
+          "rotation": 0,
+          "target": "0-fachada-"
+        },
+        {
+          "yaw": -2.0995937524694597,
+          "pitch": 0.5643628070019666,
+          "rotation": 0,
+          "target": "2-parqueadero-bbq-"
+        },
+        {
+          "yaw": -1.0359661378177627,
+          "pitch": 0.3057932601106721,
+          "rotation": 0,
+          "target": "5-oficina-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-parqueadero-bbq-",
+      "name": "PARQUEADERO BBQ-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9041487177897611,
+          "pitch": 0.4858954672194393,
+          "rotation": 0,
+          "target": "1-parqueadero-zen-"
+        },
+        {
+          "yaw": -1.0310573947699275,
+          "pitch": 0.263904907087797,
+          "rotation": 0,
+          "target": "5-oficina-"
+        },
+        {
+          "yaw": 1.177358812284643,
+          "pitch": 0.14869488768417938,
+          "rotation": 0,
+          "target": "0-fachada-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8136029828259872,
+          "pitch": 0.49921065694977784,
+          "title": "OPCION JARDIN ZEN",
+          "text": "Jardin Zen"
+        }
+      ]
+    },
+    {
+      "id": "3-sala-comedor-",
+      "name": "SALA COMEDOR-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.520763187925489,
+          "pitch": 0.20719461733543731,
+          "rotation": 0,
+          "target": "0-fachada-"
+        },
+        {
+          "yaw": 2.75664338075578,
+          "pitch": 0.22571451701552725,
+          "rotation": 0,
+          "target": "5-oficina-"
+        },
+        {
+          "yaw": -3.1224631612105114,
+          "pitch": 0.25375202563528276,
+          "rotation": 0,
+          "target": "4-cocina-"
+        },
+        {
+          "yaw": 2.5081890247427836,
+          "pitch": 0.25330719791874756,
+          "rotation": 0,
+          "target": "6-pasillo-2-piso-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cocina-",
+      "name": "COCINA-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.277852520168164,
+          "pitch": 0.36164059280492467,
+          "rotation": 0,
+          "target": "5-oficina-"
+        },
+        {
+          "yaw": 1.3270647101969324,
+          "pitch": 0.22721225888158614,
+          "rotation": 0,
+          "target": "6-pasillo-2-piso-"
+        },
+        {
+          "yaw": 0.4948922790870487,
+          "pitch": 0.21467440335334942,
+          "rotation": 0,
+          "target": "3-sala-comedor-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-oficina-",
+      "name": "OFICINA-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.997557686602736,
+          "pitch": 0.2848236089567351,
+          "rotation": 0,
+          "target": "2-parqueadero-bbq-"
+        },
+        {
+          "yaw": -0.45383248946313337,
+          "pitch": 0.2093714212084521,
+          "rotation": 0,
+          "target": "3-sala-comedor-"
+        },
+        {
+          "yaw": -0.8355136430793806,
+          "pitch": 0.2710730109898236,
+          "rotation": 0,
+          "target": "4-cocina-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pasillo-2-piso-",
+      "name": "PASILLO 2 PISO-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0767304890388196,
+          "pitch": 0.3415301572360683,
+          "rotation": 0,
+          "target": "9-habitacion-2-"
+        },
+        {
+          "yaw": 1.563955778615444,
+          "pitch": 0.5401592188000564,
+          "rotation": 0,
+          "target": "4-cocina-"
+        },
+        {
+          "yaw": 0.19731756768314312,
+          "pitch": 0.19062418177188079,
+          "rotation": 0,
+          "target": "7-habitacion-principal-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-habitacion-principal-",
+      "name": "HABITACION PRINCIPAL-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9744439914399852,
+          "pitch": 0.3040143641504045,
+          "rotation": 0,
+          "target": "8-bao-principal-"
+        },
+        {
+          "yaw": 0.3637339078417625,
+          "pitch": 0.27588926590737906,
+          "rotation": 0,
+          "target": "6-pasillo-2-piso-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bao-principal-",
+      "name": "BAÑO PRINCIPAL-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.43505424547834,
+          "pitch": 0.38963826849078487,
+          "rotation": 0,
+          "target": "7-habitacion-principal-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-habitacion-2-",
+      "name": "HABITACION 2-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7513114323848562,
+          "pitch": 0.26713659309142557,
+          "rotation": 0,
+          "target": "6-pasillo-2-piso-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SANTA ELENA LUXURY HOMES",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
